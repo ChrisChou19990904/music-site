75cb087b664be5cefb68d15e1279c46af5df1bb5
Chris Chou <D1397102@o365.fcu.edu.tw>
2025-08-26 19:50:47 +0800

    asd

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,17 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import path from 'path'  // 記得引入 path
 
 export default defineConfig({
   plugins: [vue()],
   base: '/music-site/',
   server: {
-    port: 3000, // 你可以改成喜歡的端口
-  }
+    port: 3000,
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),  // 加上這一行，設定 @ 指向 src 目錄
+    },
+  },
 })
 
