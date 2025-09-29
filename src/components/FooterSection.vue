97fca6c98f72440857a8f7e7bf2b8477b254694a
Chris Chou <D1397102@o365.fcu.edu.tw>
2025-09-29 21:34:32 +0800

    asd

--- src/components/FooterSection.vue
+++ src/components/FooterSection.vue
@@ -7,3 +7,60 @@
     <img width="540" height="720" src="/src/components/my_appearance2.jpg" alt="自拍" />
   </footer>
 </template>
+<style scoped>
+footer {
+  text-align: center;
+  background-color: purple;
+  color: yellow;
+  font-size: 50px;
+  padding: 20px;
+}
+
+a{
+  color: white;
+  text-decoration: none;
+}
+a :hover{
+  text-decoration: underline;
+}
+a :visited{
+  color: white;
+}
+
+img {
+  width: 540px;
+  height: 720px;
+  margin-top: 20px;
+}
+
+/* 響應式設計 */
+@media (max-width: 768px) {
+  footer {
+    font-size: 30px; /* 螢幕小時，字體變小 */
+  }
+
+  img {
+    width: 300px; /* 小螢幕時縮小圖片 */
+    height: 400px;
+  }
+
+  p {
+    font-size: 18px; /* 小螢幕時調整段落字體大小 */
+  }
+}
+
+@media (max-width: 480px) {
+  footer {
+    font-size: 20px; /* 更小螢幕再縮小字體 */
+  }
+
+  img {
+    width: 250px;
+    height: 350px; /* 小螢幕下圖片再次縮小 */
+  }
+
+  p {
+    font-size: 16px; /* 更小螢幕的文字調整 */
+  }
+}
+</style>
